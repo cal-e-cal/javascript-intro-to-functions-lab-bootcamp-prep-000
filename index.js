0c9cd22957e8258a5b0fa91b7c3d9ec205441320
cyu8110@gmail.com <cyu8110@gmail.com>
2018-08-28 18:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma() {
   if (string === string.toLowerCase) {
     console.log("I can't hear you!")
-  } 
-  else if (string === string.toUpperCase) {
+  } else if (string === string.toUpperCase) {
     console.log("YES INDEED")
-  }
-  else if (string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     console.log("I love you, too.")
   }
 }
